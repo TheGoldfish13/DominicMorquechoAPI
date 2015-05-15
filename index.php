9029301f05dab60546a4578e42efdc8c7e6fd26e
DominicMorquecho <dominic.morquecho@gmail.com>
2015-05-14 22:54:02 -0700

    Made tumblr-esque beautifacation of roger

--- index.php
+++ index.php
@@ -35,22 +35,22 @@ function getUserID($userName){
 }
 //function to print out images to our screen 
 function printImages($userID){
-	$url = 'https://api.instagram.com/v1/users/' . $userID . '/media/recent?client_id=' . clientID . '&count=5'; /*request last 5 picsfrom ig*/
+	$url = 'https://api.instagram.com/v1/users/' . $userID . '/media/recent?client_id=' . clientID . '&count=2'; /*request last 5 picsfrom ig*/
 	$instagramInfo = connectToInstagram($url);
 	$results = json_decode($instagramInfo, true);
 	//parse through the info one at a time
 	foreach ($results['data'] as $items) {
 		$image_url = $items['images']['low_resolution']['url']; /*go through results and bring back url of pics to save in PHP server*/
-		echo '<img src=" '.$image_url.'"/><br/>';
+		echo '<img src=" '.$image_url.'"class="test"/><br/>';
 		//callin a function tp save $image_url
 		savePictures($image_url);
 	}
 }
 //creating a function to save image to server
 function savePictures($image_url){
-	echo $image_url . '<br>';
+	/*echo $image_url . '<br>';*/
 	$filename = basename($image_url);  /*the filename is what we storing basename is PHP built in method that we using to store $image_url*/
-	echo $filename . '<br>';
+	/*echo $filename . '<br>';*/
 
 	$destination = ImageDirectory . $filename; /*making sure image doesnt exist in the storage*/
 	file_put_contents($destination, file_get_contents($image_url)); /*grabs imagefile and storesit in server*/
@@ -58,7 +58,7 @@ function savePictures($image_url){
 
 
 
-if(isset($_GET['code'])){ /*setting up array to acess ['code']*/
+if(isset($_GET['code'])){ /*setting up array to acess ['code']*/  
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
 	$access_token_settings = array('client_id' => clientID, 
@@ -75,6 +75,76 @@ curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); /*setting the po
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); /*setting it equal to 1 because we are getting strings back*/
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); /*but in live work-production we want this to be true*/
 
+?>
+ <html>
+ <head>
+ 	<title> Roger senpai!!!!</title>
+ 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+ 	<link rel="stylesheet" type="text/css" href="css/main.css"/>
+ 	<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+ 	<link href='http://fonts.googleapis.com/css?family=Redressed' rel='stylesheet' type='text/css'>
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<style> body, a, a:hover { cursor:url('http://www.dolliehost.com/dolliecrave/cursors/cursors-cute/cute63.gif'), auto }</style>
+ 	<style>
+	::-webkit-scrollbar {
+	width: 20px;
+	height: 7px;
+	background-color:#ffffff;
+	}
+	::-webkit-scrollbar-button:start:decrement,
+	::-webkit-scrollbar-button:end:increment {display: block;}
+	::-webkit-scrollbar-button:vertical:start:increment,
+	::-webkit-scrollbar-button:vertical:end:decrement {display: none;}
+	::-webkit-scrollbar-button:end:increment 
+        {background-image:url(http://www.dolliehost.com/dolliecrave/scrollbars/arrowdown.png);
+	background-repeat: no-repeat;background-position:center;
+        -webkit-box-shadow: inset 0 0 16px rgba(214,214,214,0.9);}
+</style>
+<div id="scrollbardolliecrave">
+<a href="http://www.dolliecrave.com/pretty-scrollbars.php" target="_blank" title="Tumblr Scrollbars"><img src="http://static.tumblr.com/wgdkvue/CAMmhnuc3/scrollbars.png" border=0 alt="Tumblr Scrollbars"></a></div>
+<style>
+	::-webkit-scrollbar-button:start:decrement 
+	{background-image: url(http://www.dolliehost.com/dolliecrave/scrollbars/arrowup.png);
+	background-repeat: no-repeat;background-position:center;
+        -webkit-box-shadow: inset 0 0 16px rgba(214,214,214,0.9);}
+	::-webkit-scrollbar-thumb {
+	background-color:transparent;
+        -webkit-box-shadow: inset 0 0 16px rgba(192,192,192,0.9);
+        background:url(http://www.dolliehost.com/dolliecrave/backgrounds/pretty2/59.gif); 
+        background-size: 300% auto;
+	border-left:0px solid #E5E5E5;
+	border-right:0px solid #E5E5E5;
+	border-bottom:0px solid #E5E5E5;
+	border-top:0px solid #E5E5E5;
+	border-radius:0%;
+	}
+	#scrollbardolliecrave {
+	width:96px;
+	height:20px;
+	position: fixed;    
+	left: 0px;    
+	bottom: 0px;
+	}
+	::-webkit-scrollbar-track {
+        background:url(http://www.dolliehost.com/dolliecrave/scrollbars/track.png); 
+	border-radius:0%;
+	}
+</style>
+
+ </head>
+ <body>
+ 	<img src="http://media.giphy.com/media/h7KMmkYUcbPQQ/giphy.gif" id="bunny">
+ 	<div id="rogertrust">
+ 		In Roger Kim We Trust
+ 	</div>
+	<img src="http://media.giphy.com/media/h7KMmkYUcbPQQ/giphy.gif" id="rightbunny">
+
+ <!-- login for people to aprove our app access to instagram
+ after getting aproval we have info to play with -->
+ 
+ </body>
+ </html>
+ <?php
 
 $result = curl_exec($curl);
 curl_close($curl); /*close the session*/
@@ -86,6 +156,26 @@ $userName = $results['user']['username'];
 $userID = getUserID($userName);
 
 printImages($userID);
+?>
+	<html>
+	<body>
+		<div id="rogerpoem">
+			What is Reality? <br>
+			Roger Kim is reality <br>
+			He is our religion <br>
+			He is our God <br>
+			He has a great bod <br>
+			and a holy rod <br>
+			he is great in bed <br>
+			it would be an honor <br>
+			to give him head <br>
+			No Kpopstar can measure up to him <br>
+			The one and only ROGER KIM. <br>
+			-Reimi Mosses
+		</div>
+	</body>
+	</html>
+<?php 
 }
 
 else{
@@ -95,9 +185,10 @@ else{
  <!DOCTYPE html>
  <html>
  <head>
+ 	<title> Roger senpai!</title>
+ 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+ 	<link rel="stylesheet" type="text/css" href="css/main.css"/>
  	<meta name="viewport" content="width=device-width, initial-scale=1">
- 	<title></title>
- 	<!-- <link rel="stylesheet" href="css/main.css"> -->
  </head>
  <body>
  <!-- login for people to aprove our app access to instagram
